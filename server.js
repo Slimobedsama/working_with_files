a0639321c2bde6b891117cb9c50445e1097da897
Slimobedsama <ohwo.okoro@yahoo.com>
2024-05-24 03:35:47 +0100

    created a 404 page and folder setup for reading html files

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const { Console } = require('console');
 const { createServer } = require('http');
+const { readFile } = require('fs');
 
 const PORT = 8800;
 
 const server = createServer((req, res)=> {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end();
+    res.setHeader('Content-Type', 'text/html');
+
+    let folder = './html/';
+    if(req.url == '/') {
+        res.statusCode = 201;
+        folder += 'index.html';
+    } else {
+        res.statusCode = 404;
+        folder += '404.html';
+    }
+    readFile(folder, (err, data)=> {
+        if(err) throw err;
+        res.end(data);
+    })
 });
 
 server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
